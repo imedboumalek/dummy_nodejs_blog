5f10ee0c05560429147059a727b2cfc7b9a83fff
Imed Boumalek <b.imed@spherewebservices.com>
2021-04-02 23:13:19 +0100

    added some middleware

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
 const express = require('express');
+const morgan = require('morgan');
+
 // initialize express
 const app = express();
+const port = 3000;
 
-app.listen(3000);
+// register view engine
+app.set('view engine', 'ejs');
+
+app.listen(port);
+
+app.use(express.static(__dirname + '/dist'));
+app.use(morgan('dev'));
+// app.use((req, res, next) => {
+//     console.log("new request was made:");
+//     console.log("host: ", req.hostname);
+//     console.log("url: ", req.url);
+//     console.log("path: ", req.path);
+//     console.log("method: ", req.method);
+//     next();
+// })
 app.get('/', (req, res) => {
 
-    console.log(req.url);
-    res.sendFile("./views/index.html", {
-        root: __dirname,
-    })
+    res.status(200).render('index', {
+        title: 'home'
+    });
 })
 app.get('/about', (req, res) => {
-    console.log(req.url);
-
-    res.sendFile("./views/about.html", {
-        root: __dirname,
+    res.status(200).render('about', {
+        title: 'about'
     })
 })
-app.get('/about-me', (req, res) => {
-    console.log(req.url);
 
-    res.redirect("./views/about.html")
-})
 // 404 page
 app.use((req, res) => {
-    console.log(req.url);
 
-    res.sendFile("./views/404.html", {
-        root: __dirname
-    })
+    res.status(404).render('404', {
+        title: '404'
+    });
 })
